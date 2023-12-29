9009f3046392cbf3b6d2cd37c45a4341f086e1e3
Hoosus <823546256@qq.com>
2023-12-29 15:45:47 +0800

    Add config

--- train.py
+++ train.py
@@ -1,18 +1,27 @@
 import torch
-
+import yaml
 from data import load_lego
-from utils import get_datetime_str
+from utils import get_datetime_str, parser_parse_args
 
 
 
 if __name__ == "__main__":
+  args = parser_parse_args()
+  config_path = args.config
+  config = yaml.safe_load(open(config_path))
+  print(config)
+  name = config["name"]
+  assert name in ["lego"]
+  data_path = config["data_path"]
+  exp_name = config["exp_name"]
+  log_path = f"./logs/{exp_name}_{get_datetime_str()}"
 
-  lego_path = './data/lego'
-  log_path = f"./logs/lego_{get_datetime_str()}"
+  if name == "lego":
+    dataset = {
+      'train' : load_lego(data_path, 'train', samples=[26, 86, 2, 55]),
+      'val' : load_lego(data_path, 'val', skip=8),
+      'test' : load_lego(data_path, 'test', skip=8)
+    }
   
-  dataset = {
-    'train' : load_lego(lego_path, 'train', samples=[26, 86, 2, 55]),
-    'val' : load_lego(lego_path, 'val', skip=8),
-    'test' : load_lego(lego_path, 'test', skip=8)
-  }
+  
   
--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import datetime
+import argparse
 
 def get_datetime_str():
   return datetime.datetime.now().strftime("date:%Y-%m-%d_%H:%M:%S")
+
+def parser_parse_args():
+  parser = argparse.ArgumentParser(description="JInfoNerf")
+  parser.add_argument('--config', nargs=1, type=str, default="./configs/lego.yaml")
+  return parser.parse_args()
